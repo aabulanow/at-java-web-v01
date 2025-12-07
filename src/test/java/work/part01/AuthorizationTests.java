09554ce18980832c10821dde6754d960c8ff38af
Andrei Bulanov <abulanov337@gmail.com>
2025-12-07 16:38:18 +0300

    xpath

--- src/test/java/work/part01/AuthorizationTests.java
+++ src/test/java/work/part01/AuthorizationTests.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 class AuthorizationTests {
     @Test
     public void test01LoginSuccess() {
         open("https://slqamsk.github.io/cases/slflights/v01/");
-        SelenideElement myElement = $("input[name+username]");
         $(By.id("username")).sendKeys("standard_user");
         $(By.id("password")).type("stand_pass1");
         $(By.id("loginButton")).click();
@@ -22,10 +24,16 @@ class AuthorizationTests {
     @Test
     public void test02LoginWrongPassword() {
         open("https://slqamsk.github.io/cases/slflights/v01/");
-        SelenideElement myElement = $("input[name+username]");
         $(By.id("username")).sendKeys("standard_user");
         $(By.id("password")).type("stand_pass11");
         $(By.id("loginButton")).click();
         $("body").shouldHave(text("Неверное имя пользователя или пароль."));
+    }
+
+    @Test
+    public void testPageH1() {
+        open("https://slqamsk.github.io/tmp/xPath01.html");
+        $x("//h1").shouldHave(text("Учебная страница для XPath"));
+        $x("//h1").shouldHave(exactText("Учебная страница для XPath"));
     }
 }
