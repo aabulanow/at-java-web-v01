348312e846d777d93c6527909d340c4cd5d717ed
unknown <a.a.bulanow@gmail.com>
2025-12-18 19:31:22 +0400

    ДЗ урока 3

--- src/test/java/work/part01/HomeTasks2.java
+++ src/test/java/work/part01/HomeTasks2.java
@@ -2,52 +2,112 @@ package work.part01;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Condition.clickable;
+import org.junit.jupiter.params.ParameterizedTest;
+import io.qameta.allure.Allure;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+
+
 public class HomeTasks2 {
+    @BeforeEach
+    void setUp() {
+        Configuration.browser = "firefox";
+    }
+
     @Test
     void test01SimpleModal() {
         Configuration.pageLoadStrategy = "eager";
         open("https://www.specialist.ru/");
         $x("//*[@id='cookieConsent__ok']").shouldBe(exist);
         $x("//*[@id='cookieConsent__ok']").click();
-        sleep(1000);
         $x("//a[contains(text(),'Форматы обучения')]").click();
-        sleep(1000);
         $x("//a[contains(text(),'Свободное обучение')]").click();
-        sleep(1000);
         $x("//a[@class='page-button banner-button']").click();
-        sleep(1000);
         $x("//select[@id='Filter_CategoriesDirectionFilter']").click();
-        sleep(1000);
         $x("//button[@id='sendBtn']").click();
         $x("//a[contains(text(),'Тестирование ПО. Уровень 1. Тестировщик программно')]").shouldBe(exist);
     }
-
-    //a[contains(text(),'Тестирование ПО. Уровень 1. Тестировщик программно')]
     @Test
-    void test02SimpleModal() {
+    void test01() {
         Configuration.pageLoadStrategy = "eager";
-        open("https://practice-automation.com/modals/");
-        $x("//div[@id='popmake-1318']").shouldBe(exist);
-        $("#simpleModal").click();
+        open("http://92.51.36.108:7777/sl.qa/cinema/index.php");
+        $x("//input[@name='age']").sendKeys("12");
+        String dateValue = "16-12-2025";
+        $x("//input[@name='date']").setValue(dateValue);
+        $x("//input[@value='8']").click();
+        $x("//input[@value='back']").click();
+        $x("//input[@value='Рассчитать']").click();
+        $x("//div[contains(text(),':')]").shouldHave(text("Стоимость билета: 400 рублей."));
         sleep(1000);
-        $x("//div[@id='popmake-1318']//button")
-                .shouldBe(visible)
-                .shouldBe(clickable)
-                .click();
-
-        $x("//div[@id='popmake-1318']//button").shouldBe(visible);
-        $x("//div[@id='popmake-1318']//button").shouldBe(clickable);
-        $x("//div[@id='popmake-1318']//button").click();
-
-
-
-        sleep(2_000);
-        $x("//a[text()='Home']").click();
-        sleep(2_000);
     }
+    @Test
+    void test02() {
+        Configuration.pageLoadStrategy = "eager";
+        open("http://92.51.36.108:7777/sl.qa/cinema/index.php");
+        $x("//input[@name='age']").sendKeys("18");
+        String dateValue = "16-12-2025";
+        $x("//input[@name='date']").setValue(dateValue);
+        $x("//input[@value='10']").click();
+        $x("//input[@value='crime']").click();
+        $x("//input[@value='Рассчитать']").click();
+        $x("//div[contains(text(),':')]").shouldHave(text("Стоимость билета: 400 рублей."));
+    }
+    @Test
+    void test03() {
+        Configuration.pageLoadStrategy = "eager";
+        open("http://92.51.36.108:7777/sl.qa/cinema/index.php");
+        $x("//input[@name='age']").sendKeys("12");
+        String dateValue = "16-12-2025";
+        $x("//input[@name='date']").setValue(dateValue);
+        $x("//input[@value='10']").click();
+        $x("//input[@value='crime']").click();
+        $x("//input[@value='Рассчитать']").click();
+        $x("//div[contains(text(),':')]").shouldHave(text("Стоимость билета: *Этот фильм можно смотреть только с 18 лет*"));
+    }
+
+    @Test
+    void test04() {
+        Configuration.pageLoadStrategy = "eager";
+        open("http://92.51.36.108:7777/sl.qa/cinema/index.php");
+        $x("//input[@name='age']").sendKeys("12");
+        String dateValue = "16-12-2025";
+        $x("//input[@name='date']").setValue(dateValue);
+        $x("//input[@value='1']").click();
+        $x("//input[@value='king']").click();
+        $x("//input[@value='Рассчитать']").click();
+        $x("//div[contains(text(),':')]").shouldHave(text("Стоимость билета: 350 рублей."));
+    }
+    @Test
+    void test05() {
+        Configuration.pageLoadStrategy = "eager";
+        open("http://92.51.36.108:7777/sl.qa/cinema/index.php");
+        $x("//input[@name='age']").sendKeys("17");
+        $x("//input[@name='date']").sendKeys("25");
+        $x("//input[@value='1']").click();
+        $x("//input[@value='tango']").click();
+        $x("//input[@value='Рассчитать']").click();
+        $x("//div[contains(text(),':')]").shouldHave(text("Стоимость билета: *Этот фильм можно смотреть только с 18 лет*"));
+    }
+
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "data.csv", numLinesToSkip = 1)
+    void test02_param_cinema(String age, String dateValue, String start, String film, String priceMessage) {
+        open("http://92.51.36.108:7777/sl.qa/cinema/index.php");
+        $("input[name=age]").setValue(age);
+        $("input[name='date']").setValue(dateValue); // Дата в формате YYYY-MM-DD
+        $("input[name=session][value='" + start + "']").click();
+        $("input[name=film][value=" + film + "]").click();
+        $("input[type=submit").click();
+        $("div").shouldHave(text(priceMessage));
+        sleep(1_000);
 }
---
+++ src/test/java/work/part01/LocalDate.java
@@ -0,0 +1,4 @@
+package work.part01;
+
+public class LocalDate {
+}
